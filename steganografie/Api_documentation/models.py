f8439c6f34c1463675c12d448ee3d6d3dd3828c3
Kallo Alexandru Vlad <vlad807@yahoo.com>
2023-06-21 21:42:01 +0300

    CV2

--- steganografie/Api_documentation/models.py
+++ steganografie/Api_documentation/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Photo(models.Model):
+
+    email = models.EmailField("Enter Email")
+    file = models.FileField(upload_to="cache")
+    message = models.CharField("Enter message", max_length=150, default="")
--- steganografie/Api_documentation/views.py
+++ steganografie/Api_documentation/views.py
@@ -2,32 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-foto = [
-    {
-        'id': 1,
-        'Nume': 'Harry Potter',
-        'Status': True
-    },
-    {
-
-        'id': 2,
-        'Nume': 'Spiderman',
-        'Status': False
-
-    },
-    {
-
-        'id': 3,
-        'Nume': 'Copilarie',
-        'Status': True
-
-    }
-]
-
-
 def home(request):
-    context = {'foto': foto}
-    return render(request, 'Api_documentation/home.html', context)
+    return render(request, 'Api_documentation/home.html')
 
 
 def criptare(request):
